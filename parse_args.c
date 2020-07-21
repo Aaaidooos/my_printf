59fb345ab753983a6a24a0886ae5ab8128f6903c
Aidos <66068633+Aaaidooos@users.noreply.github.com>
2020-07-21 18:29:34 +0600

    refactoring

--- parse_args.c
+++ parse_args.c
@@ -7,22 +7,21 @@ int parse_options(va_list ap, char sym)
   {
     case 's' : size += parse_string(ap);break;
     case 'c' : size += parse_char(ap);break;
-    case 'd' : size += parse_decimal(ap);break;
-    case 'i' : size += parse_decimal(ap);break;
+    case 'd' : size += itoa_base(ap, 10);break;
+    case 'i' : size += itoa_base(ap, 10);break;
     case 'u' : size += parse_unsigned(ap);break;
-    case 'o' : size += parse_octal(ap);break;
-    case 'x' : size += parse_hex(ap);break;
+    case 'o' : size += itoa_base(ap, 8);break;
+    case 'x' : size += itoa_base(ap, 16);break;
     case 'p' : size += parse_ptr(ap);break;
     case '%' : size += put_char('%');break;
   }
   return size;
 }
 
-
 int parse_args(va_list ap, char* format)
 {
      int size = 0;
-     while(*format != 0)
+     while(*format != 0) // 0 == '/0'
      {
          if(*format == '%')
          {
