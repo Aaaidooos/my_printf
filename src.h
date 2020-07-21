7a995de155da07129b6a8a2d967a401a067432e5
Aidos <66068633+Aaaidooos@users.noreply.github.com>
2020-07-21 18:30:59 +0600

    Update src.h

--- src.h
+++ src.h
@@ -16,7 +16,20 @@ int parse_decimal(va_list ap);
 char* convert_to(long num, int base);
 int print_number(long num, int base);
 int parse_unsigned(va_list ap);
-int parse_octal(va_list ap);
-int parse_hex(va_list ap);
 int parse_ptr(va_list ap);
 char* make_ptr(long val);
+int itoa_base(va_list ap, int base);
+
+// test functions
+void test_0_1();
+void test_2_3();
+void test_4_5();
+void test_6_7();
+void test_8_9();
+void test_10_11();
+void test_12_13();
+void test_14_15();
+void test_16_17();
+void test_18_19();
+void test_20_21();
+void test_22_23();
